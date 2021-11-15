829a7be0a3b3384d82b4ba8991d20d9f925f1d64
the-electro-punk <bwr2555@hotmail.com>
2021-11-15 13:04:34 -0600

    fixed routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,6 +7,15 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category,
+    {
+      model: Tag,
+      through: ProductTag,
+    }]
+  }).then((response) => {
+    res.json(response)
+  })
 });
 
 // get one product
